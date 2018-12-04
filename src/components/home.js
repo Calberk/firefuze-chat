6e9090b76c77ae30e0bda072afd16ec61925cab2
Calberk <park.edmund@gmail.com>
2018-12-04 13:36:39 -0800

    setup 404 page and chat pages

---
+++ src/components/home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default props => (
+    <div className='center'>
+        <h1>FireFUZE App</h1>
+        <p className='grey-text'>Fuel the Fire</p>
+    </div>
+    )
